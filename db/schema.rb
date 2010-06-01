397d0bf572e32f781976f270c862816be97d6337
Peter Kos <peterkos@peter-kos-computer.local>
2010-05-31 22:52:46 -0500

    done with user edit/update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100601005155) do
+ActiveRecord::Schema.define(:version => 20100601032456) do
 
   create_table "references", :force => true do |t|
     t.string   "title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20100601005155) do
     t.string   "encrypted_password"
     t.string   "salt"
     t.string   "remember_token"
+    t.boolean  "admin",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
