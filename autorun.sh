7a9b080d43f5fc12016899805675e8b1842463c7
Peter Kos <peterkos@peter-kos-computer.local>
2010-07-02 02:41:38 -0500

    removed deployment

--- autorun.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mysqladmin -u root password pointone
-mysqladmin -u root -ppointone create references_development
-mysql references_development --user=root -ppointone < user.sql
-rake db:migrate:reset
